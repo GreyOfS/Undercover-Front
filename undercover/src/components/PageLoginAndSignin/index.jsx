4e8a1d6653941cee38340109b16eface00f66954
GreyOfS <jeanandre.beuf+github@gmail.com>
2023-04-03 16:05:11 +0200

    media query

--- undercover/src/components/PageLoginAndSignin/index.jsx
+++ undercover/src/components/PageLoginAndSignin/index.jsx
@@ -9,10 +9,22 @@ import { useForm } from 'react-hook-form';
 import { NavLink } from 'react-router-dom';
 
 function SigninTab() {
-    // const { register } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [errorsList, setErrorsList] = useState([]);
     return(
-        <form className='signinTab'>
-
+        <form className='signinTab' onSubmit={handleSubmit((data) => {
+            console.log(data, errors);
+        })}>
+            <Input type='email' register={{...register('email', { required: "The email is required" })}} tag='email' />
+            <Input type='text' register={{...register('name', { required: "The name is required" })}} tag='name' />
+            <Input type='password' register={{...register('password1', { required: "The password is required" })}} tag='password' />
+            <Input type='password' register={{...register('password2', { required: "The confirmation password is required" })}} tag='confirmation' />
+            <span>
+                <input type="checkbox" register={{...register('cguAccepted')}} />
+                <label>I accept the general conditions of use</label>
+            </span>
+            <ErrorsMessage codes={errorsList} />
+            <Button type='submit'>Sign In</Button>
         </form>
         )
 }
